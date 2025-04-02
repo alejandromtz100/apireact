122aecd0f40bf3dcbb2dcf9f484df260d303c790
alejandromtz100 <alejandro.martinez.22s@utzmg.edu.mx>
2025-04-02 17:16:46 -0600

    Recuperar contraseña agregando email

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  phoneNumber: { type: String, required: true},
+  phoneNumber: { type: String, required: true },
+  email: { type: String, required: true, unique: true }, // Campo de correo agregado
   department: { type: String, required: true },
   tower: { type: String, required: true },
   password: { type: String, required: true },
